8a69da5a165e7f86cfc5bbb2f4139a8d58341b0f
jiangbaoi <jiangbaozi@seczone.cn>
2022-08-10 10:11:45 +0800

    多模块

---
+++ api/src/main/java/com/demo/App.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import com.demo.util.StringUtil;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        StringUtil.getString();
+    }
+}
---
+++ common/src/main/java/com/demo/util/StringUtil.java
@@ -0,0 +1,8 @@
+package com.demo.util;
+
+public class StringUtil {
+
+    public static void getString() {
+        System.out.println("hello");
+    }
+}
